54d7a39f6fdded5fa44be3987d111868f848eba2
Diego Medina <diegurrio@gmail.com>
2021-02-12 14:29:20 -0800

    finish affirmations

--- affirmations/src/main/java/com/diego/android/kitchen/affirmations/model/Affirmation.kt
+++ affirmations/src/main/java/com/diego/android/kitchen/affirmations/model/Affirmation.kt
@@ -1,6 +1,13 @@
 package com.diego.android.kitchen.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 /**
  * Simple data class to store a String resource ID.
  */
-data class Affirmation(val stringResourceId: Int)
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
+
