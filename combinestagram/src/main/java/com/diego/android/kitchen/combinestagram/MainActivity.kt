4269b95ee173c765a7f5d6ea1a2f056126a282e2
Diego Medina <diego.medina@eargo.com>
2021-03-12 15:08:16 -0800

    add listening to subject

--- combinestagram/src/main/java/com/diego/android/kitchen/combinestagram/MainActivity.kt
+++ combinestagram/src/main/java/com/diego/android/kitchen/combinestagram/MainActivity.kt
@@ -1,46 +1,63 @@
-
 package com.diego.android.kitchen.combinestagram
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-  private lateinit var viewModel: SharedViewModel
+    private lateinit var viewModel: SharedViewModel
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    title = getString(R.string.collage)
+        title = getString(R.string.collage)
 
-    viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
 
-    addButton.setOnClickListener {
-      actionAdd()
+        addButton.setOnClickListener {
+            actionAdd()
+        }
+
+        clearButton.setOnClickListener {
+            actionClear()
+        }
+
+        saveButton.setOnClickListener {
+            actionSave()
+        }
+
+        // Add an observer to the LiveData object in the view model
+        viewModel.getSelectedPhotos().observe(this, Observer { photos ->
+            photos?.let {
+                if (it.isNotEmpty()) {
+                    val bitmaps =
+                        photos.map { BitmapFactory.decodeResource(resources, it.drawable) }
+                    val newBitmap = combineImages(bitmaps)
+                    collageImage.setImageDrawable(BitmapDrawable(resources, newBitmap))
+                } else {
+                    actionClear()
+                }
+            }
+        })
     }
 
-    clearButton.setOnClickListener {
-      actionClear()
+    private fun actionAdd() {
+        viewModel.addPhoto(PhotoStore.photos[0])
     }
 
-    saveButton.setOnClickListener {
-      actionSave()
+    private fun actionClear() {
+        viewModel.clearPhoto()
+        collageImage.setImageResource(android.R.color.transparent)
     }
-  }
 
-  private fun actionAdd() {
-    println("actionAdd")
-  }
-
-  private fun actionClear() {
-    println("actionClear")
-  }
-
-  private fun actionSave() {
-    println("actionSave")
-  }
+    private fun actionSave() {
+        println("actionSave")
+    }
 }
--- combinestagram/src/main/java/com/diego/android/kitchen/combinestagram/SharedViewModel.kt
+++ combinestagram/src/main/java/com/diego/android/kitchen/combinestagram/SharedViewModel.kt
@@ -35,7 +35,11 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.util.Log
 import android.widget.ImageView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -44,26 +48,55 @@ import java.io.OutputStream
 
 class SharedViewModel : ViewModel() {
 
-  fun saveBitmapFromImageView(imageView: ImageView, context: Context) {
-    val tmpImg = "${System.currentTimeMillis()}.png"
+    private val selectedPhotos = MutableLiveData<List<Photo>>()
+    private val subscriptions = CompositeDisposable()
+    private val imagesSubject: BehaviorSubject<MutableList<Photo>> = BehaviorSubject.createDefault(
+        mutableListOf()
+    )
 
-    val os: OutputStream?
-
-    val collagesDirectory = File(context.getExternalFilesDir(null), "collages")
-    if (!collagesDirectory.exists()) {
-      collagesDirectory.mkdirs()
+    init {
+        subscriptions.add(imagesSubject.subscribe {
+            selectedPhotos.value = imagesSubject.value
+        })
     }
 
-    val file = File(collagesDirectory, tmpImg)
-
-    try {
-      os = FileOutputStream(file)
-      val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-      bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
-      os.flush()
-      os.close()
-    } catch(e: IOException) {
-      Log.e("MainActivity", "Problem saving collage", e)
+    override fun onCleared() {
+        super.onCleared()
+        subscriptions.dispose()
     }
-  }
+
+    fun addPhoto(photo: Photo) {
+        // Add a new photo to the subject, which will emit a new list to all subscribers.
+        imagesSubject.value.add(photo)
+        imagesSubject.onNext(imagesSubject.value)
+    }
+
+    fun clearPhoto() {
+        imagesSubject.value.clear()
+    }
+
+    fun getSelectedPhotos(): LiveData<List<Photo>> = selectedPhotos
+
+    fun saveBitmapFromImageView(imageView: ImageView, context: Context) {
+        val tmpImg = "${System.currentTimeMillis()}.png"
+
+        val os: OutputStream?
+
+        val collagesDirectory = File(context.getExternalFilesDir(null), "collages")
+        if (!collagesDirectory.exists()) {
+            collagesDirectory.mkdirs()
+        }
+
+        val file = File(collagesDirectory, tmpImg)
+
+        try {
+            os = FileOutputStream(file)
+            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, os)
+            os.flush()
+            os.close()
+        } catch (e: IOException) {
+            Log.e("MainActivity", "Problem saving collage", e)
+        }
+    }
 }
